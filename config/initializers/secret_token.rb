69db268dab05ad6306d94c16a9b78a385074d614
Ari Atkins <ari@unpluggedinstruments.com>
2012-08-08 16:10:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partytronic::Application.config.secret_token = 'a82e6b40bdc67e00064124d0b6e4baa324d922d95318aede0050327bcd0f7bf67a7b44a89837e8c5d4567f9718e2f00e4305f2c9f4dc48b76d75be1d12584760'
